7f0e63a83a18f7613e577f7c382e10695ea70ae5
Gaurav Kumar <gauravsyr10@gmail.com>
2020-11-04 23:31:07 +0530

    content added

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -16,11 +16,13 @@ if (module.hot) {
   git branch <new-branch-name> :- for creating new branches
   git checkout <branch-name>  :- to jump to that branch we want to go to
   git merge <branch-name>  :- 'branch-name' we want to merge (combine branches with  'master')
+  git merge <branch-name> --no-ff  :-  (then after hit enter, hit esc  and type type :wq and press enter)
   git push origin master 
   git commit -am "<commit message>"  :-  stage and commit in one move
   git push origin <branch-name>  :- it pushes to the "branch-name" specified (not the master branch in this case)
   git pull origin master
   git branch -d <branch-name>  :-  to delete a branch 
   git checkout -b <branch-name>  :- create a branch and jump to the new branch in one move
+  
 
 */
