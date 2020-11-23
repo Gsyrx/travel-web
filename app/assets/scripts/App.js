3cf1f1a378ab086aeb4f4263ef8af5c005fcbec9
Gaurav Kumar <gauravsyr10@gmail.com>
2020-11-23 10:18:09 +0530

    completed sticky header functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,28 +1,14 @@
 import '../styles/styles.css';
+import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
+
+let stickyHeader = new StickyHeader();
+new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
+new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
+
+let mobileMenu = new MobileMenu();
 
 if (module.hot) {
   module.hot.accept();
 }
-
-/*
-  Git and git commands:-  
-
-  git branch :- it will list all branches present 
-  git status :- 
-  git add <file-name> or
-  git add -A (to add all files to the staging area)
-  git reset <file-name>  :- remove a file from staging area(ie unstage)
-  git commit -m "some content"
-  git branch <new-branch-name> :- for creating new branches
-  git checkout <branch-name>  :- to jump to that branch we want to go to
-  git merge <branch-name>  :- 'branch-name' we want to merge (combine branches with  'master')
-  git merge <branch-name> --no-ff  :-  (then after hit enter, hit esc  and type type :wq and press enter)
-  git push origin master 
-  git commit -am "<commit message>"  :-  stage and commit in one move
-  git push origin <branch-name>  :- it pushes to the "branch-name" specified (not the master branch in this case)
-  git pull origin master
-  git branch -d <branch-name>  :-  to delete a branch 
-  git checkout -b <branch-name>  :- create a branch and jump to the new branch in one move
-  
-
-*/
